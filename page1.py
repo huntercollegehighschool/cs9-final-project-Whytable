c9178981babb9f375fbaab3ddd93de3e1620b935
ANYBODyS <93940258+Whytable@users.noreply.github.com>
2022-01-24 12:08:30 +0000

    Finished

--- page1.py
+++ page1.py
@@ -1,1 +1,9 @@
 #Use of this page is optional. If you use code here, make sure either import page1 or from page1 import * appear on your main.py page.
+#HEAL
+# a = health points
+# b = healcount
+# c = healcountmax
+
+def heal(a):
+    a = a + 1
+    return(a)
